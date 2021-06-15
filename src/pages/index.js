5eb053796c0f801f7090705155ac450d79021ef3
Felix Paradis <allo@felixparadis.com>
2021-06-15 15:07:17 -0400

    bug repro with Tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,23 @@
-import * as React from "react"
+import React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import RadioDemo from "../components/RadioDemo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <RadioDemo />
+    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
